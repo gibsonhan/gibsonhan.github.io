d167cdf3d0bd9270559a16160f03e507e915c4e8
Gibson Han <gibsonxhan@gmail.com>
2019-08-14 19:39:37 -0700

    implementing project card component without JSX

--- Project.js
+++ Project.js
@@ -2,41 +2,38 @@
 
 const e = React.createElement;
 
-
-function ProjectList(props){
+function ProjectCard (props){
+    const {img, title, description, tecStack} = props.card;
     return e(
-        'div',
-        null,
-        'Project List Place Holder'
-    )
+        'div', {key: 'Card', className: 'card'},
+        [
+            e('div', {key: 'img', className: 'image'},
+            ),
+            e('div', {key: 'box', className: 'box'},
+                [
+                    e('div', {key: 'title', className: 'title'}, title),
+                    e('div', {key: 'summary', className: 'description'}, description ),
+                    e('div', {key: 'keywords', className: 'keywords'}, tecStack)
+                ]) 
+        ]
+    );
 }
 
-function ProjectCard (prop){
-    return e(
-        'div',
-        null,
-        'Project Card'
-    )
-}
+let data = [
+            {id: 1, img: 'assets/image.png', title: 'test title', description: 'description', tecStack: ['webpack ', 'babel ', 'wordpress ']},
+            {id: 2, img: 'assets/image.png', title: 'test title', description: 'description', tecStack: ['webpack', 'babel', 'wordpress']},
+            {id: 3, img: 'assets/image.png', title: 'test title', description: 'description', tecStack: ['webpack', 'babel', 'wordpress']}
+        ]
 
 class Project extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { projects: data };
   }
-
   render() {
+    const data = this.state.projects
     return e(
-        'div', {key: 'Card', className: 'card'},
-        [
-            e('div', {key: 'image', className: 'image'}, 'img' ),
-            e('div', {key: 'box', className: 'box'},
-                [
-                    e('div', {key: 'title', className: 'title'}, 'This is the card title' ),
-                    e('div', {key: 'summary', className: 'description'}, 'Project Description for have to this is something we will to and learn how to map' ),
-                    e('div', {key: 'keywords', className: 'keywords'}, 'React, MongoDB, Javascript')
-                ]) 
-        ]
+        "div", null, data.map(data => e(ProjectCard, {key: data.id, card: data}))
     );
   }
 }
